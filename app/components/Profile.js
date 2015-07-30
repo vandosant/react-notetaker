728a178dd76d2c0a0e63939012960ac60d1c938c
vandosant <sjskender@gmail.com>
2015-07-30 13:11:23 -0600

    Added route for Profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,11 +2,20 @@ var React = require('react');
 var Router = require('react-router');
 
 var Profile = React.createClass({
+  mixins: [Router.State],
+  getInitialState: function () {
+    return {
+      notes: [],
+      bio: {},
+      repos: []
+    }
+  },
   render: function () {
+    var username = this.getParams().username;
     return (
       <div className="row">
         <div className="col-md-4">
-          User Profile
+          User Profile {username}
         </div>
         <div className="col-md-4">
           Repos
