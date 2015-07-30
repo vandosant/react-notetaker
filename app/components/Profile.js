0c32d3ee3f8ee1521b9d661a339dc1df9690bc34
vandosant <sjskender@gmail.com>
2015-07-29 20:59:56 -0600

    Begin creating Profile

---
+++ app/components/Profile.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Profile = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <div>
+        </div>
+      </div>
+    )
+  }
+});
