998c2279b8e3c21c3a46e521bb23d5ad6a688da1
vandosant <sjskender@gmail.com>
2015-08-22 13:09:47 -0600

    Formatting whitespace

--- app/components/GitHub/UserProfile.js
+++ app/components/GitHub/UserProfile.js
@@ -6,7 +6,8 @@ var UserProfile = React.createClass({
       <div>
         <h3>User Profile </h3>
         <ul className="list-group">
-          {this.props.bio.avatar_url && <li className="list-group-item"><img className="img-thumbnail" src={this.props.bio.avatar_url} /></li>}
+          {this.props.bio.avatar_url &&
+          <li className="list-group-item"><img className="img-thumbnail" src={this.props.bio.avatar_url}/></li>}
           {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
           {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
           {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
@@ -14,7 +15,8 @@ var UserProfile = React.createClass({
           {this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
           {this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
           {this.props.bio.repos && <li className="list-group-item">Repos: {this.props.bio.repos}</li>}
-          {this.props.bio.blog && <li className="list-group-item">Website: <a href={this.props.bio.blog}>{this.props.bio.blog}</a></li>}
+          {this.props.bio.blog &&
+          <li className="list-group-item">Website: <a href={this.props.bio.blog}>{this.props.bio.blog}</a></li>}
         </ul>
       </div>
     )
