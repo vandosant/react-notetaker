01af0a6c505f6bfbfaa6cb488c4d5bc742e55af1
vandosant <sjskender@gmail.com>
2015-08-23 18:47:00 -0600

    Refactoring so axios uses string literals and method shorthand

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 function getGithubRepos(username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos');
+  return axios.get(`https://api.github.com/users/${username}/repos`);
 }
 
 function getGithubUser(username) {
-  return axios.get('https://api.github.com/users/' + username);
+  return axios.get(`https://api.github.com/users/${username}`);
 }
 
 var helpers = {
-  getUserData: function (username) {
+  getUserData(username) {
     return axios.all([getGithubRepos(username), getGithubUser(username)])
-      .then(function (dataArray) {
+      .then((dataArray) => {
         return {
           repos: dataArray[0].data,
           bio: dataArray[1].data
